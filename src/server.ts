c85b112a218e2589bd178ab8293f6c5d09e00cc4
Rovani Pereira <rovani.pereira@wmw.com.br>
2021-04-21 22:11:34 -0300

    Aula 2

--- src/server.ts
+++ src/server.ts
@@ -3,16 +3,9 @@ import express from "express";
 const app = express();
 
 import './database';
+import {routes} from "./routes";
 
-app.get("/",(request, response)=>{
-    return response.json({
-        "message": "Olá mundo!"
-    });
-});
+app.use(express.json());
+app.use(routes);
 
-app.post("/users", (request, response)=>{
-    return response.json({
-        message:"Usuario cadastrado com sucesso."
-    })
-});
 app.listen(3333, ()=>console.log("O servidor está rodando na porta 3333"));
